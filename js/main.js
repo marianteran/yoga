989164db4700a5ef06a61f0d4f1b1c9f57f783dd
marianteran <marianteranf@gmail.com>
2021-09-06 12:06:34 -0300

    Configuración inicial

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.querySelector('.menu-btn').addEventListener('click', ()=>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+
+
+});
+
+ScrollReveal().reveal('.beneficios');
+ScrollReveal().reveal('.news-card',{delay:500});
+ScrollReveal().reveal('.breack',{delay:500});
+ScrollReveal().reveal('.estilos',{delay:500});
+ScrollReveal().reveal('.posicion',{delay:500});
+
+
+
+               
